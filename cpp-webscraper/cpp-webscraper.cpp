c9570c60830eac23bd0ea8c320939bbf8d0e2ef3
seahsongli <seahsongli@gmail.com>
2024-09-30 21:00:35 +0800

    Add cpp-webscraper project and initial cpp-webscraper code.

---
+++ cpp-webscraper/cpp-webscraper.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream> 
+#include <string.h>
+#include <curl/curl.h>
+#include "libxml/HTMLparser.h"
+#include "libxml/xpath.h"
+
+std::string get_request(std::string url)
+{
+    CURL* curl = curl_easy_init();
+    std::string result;
+    if (curl)
+    {
+        CURLcode res;
+
+        // setopt is used to tell libcurl how to behave.
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str()); // c_str to convert C++ string to C_Style strings. 
+        res = curl_easy_perform(curl);
+        
+        // write the data we got into the variable result
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
+        curl_easy_perform(curl);
+
+        // free up curl resources
+        curl_easy_cleanup(curl);
+    }
+    return result;
+}
+int main()
+{
+    // Initialse curl globally
+    curl_global_init(CURL_GLOBAL_ALL);
+
+    std::string html_document = get_request("https://www.scrapingcourse.com/ecommerce/");
+    std::cout << html_document;
+
+    curl_global_cleanup();
+    return 0;
+}
---
+++ cpp-webscraper/cpp-webscraper.h
@@ -0,0 +1,8 @@
+﻿// cpp-webscraper.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
